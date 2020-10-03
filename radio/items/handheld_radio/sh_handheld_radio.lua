8d088922f529c6d942e9452408347bd672bc91c6
SleepyMode <sleepy@sleepymode.me>
2020-10-03 16:41:15 +0300

    Upload radio plugin :P

---
+++ radio/items/handheld_radio/sh_handheld_radio.lua
@@ -0,0 +1,4 @@
+
+ITEM.name = "Handheld Radio"
+ITEM.model = Model("models/radio/w_radio.mdl")
+ITEM.description = "An handheld radio with a blue digital screen. It allows you to access a range of frequencies."
